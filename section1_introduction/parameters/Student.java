596d4f2f6baf38506a788d38f2fef96f45f478b4
anavilcu3 <103427540+anavilcu3@users.noreply.github.com>
2022-04-17 22:19:01 +0300

    Add files via upload

---
+++ section1_introduction/parameters/Student.java
@@ -0,0 +1,39 @@
+package section1_introduction.parameters;
+
+public class Student {
+
+    private String name;
+    private Integer averageGrade;
+
+    public Student() {
+    }
+
+    public Student(String name, Integer averageGrade) {
+        this.name = name;
+        this.averageGrade = averageGrade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAverageGrade() {
+        return averageGrade;
+    }
+
+    public void setAverageGrade(Integer averageGrade) {
+        this.averageGrade = averageGrade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", averageGrade=" + averageGrade +
+                '}';
+    }
+}
